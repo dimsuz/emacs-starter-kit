6535fa08c49b42700a759d50f31c359f0256cca2
Dmitry Suzdalev <dimsuz@gmail.com>
2012-01-11 00:28:29 +0400

    Latest changes and additions

--- dimka.el
+++ dimka.el
@@ -1,60 +1,45 @@
 (require 'color-theme)
-(load-file "~/.emacs.d/color-theme-railscasts.el")
+
+;;
+;; Generic settings
+;;
 (color-theme-zenburn)
-
-;; remove trailing whitespace
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-;; no highlight line (enabled by starterkit by default)
-(remove-hook 'coding-hook 'turn-on-hl-line-mode)
-
 (whole-line-or-region-mode 1)
+
+;;
+;; Global key bindings
 (define-key global-map (kbd "RET") 'newline-and-indent)
 (global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
 (global-set-key (kbd "M-i") 'ido-goto-symbol)
-;; (global-set-key "\C-o" 'vi-open-line-below)
-
-(require 'ruby-electric)
-(add-hook 'ruby-mode-hook 'ruby-electric-mode)
+(global-set-key (kbd "M-o") 'open-previous-line)
+(global-set-key (kbd "C-o") 'open-next-line)
 
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/auto-complete")
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "/usr/share/emacs/site-lisp/auto-complete/ac-dict")
 (ac-config-default)
 
-;; backups
-(setq
-   backup-by-copying t      ; don't clobber symlinks
-   backup-directory-alist
-    '(("." . "~/.saves"))    ; don't litter my fs tree
-   delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
-   version-control t)       ; use versioned backups
+;;
+;; Generic hook customisations
+;;
+(add-hook 'before-save-hook 'delete-trailing-whitespace) ;; remove trailing whitespace
+(remove-hook 'coding-hook 'turn-on-hl-line-mode) ;; no highlight line (enabled by starterkit by default)
 
-(defun vi-open-line-above ()
-  "Insert a newline above the current line and put point at beginning."
-  (interactive)
-  (unless (bolp)
-    (beginning-of-line))
-  (newline)
-  (forward-line -1)
-  (indent-according-to-mode))
+;;
+;; Ruby specific customizations
+;;
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook 'ruby-electric-mode)
 
-(defun vi-open-line-below ()
-  "Insert a newline below the current line and put point at beginning."
-  (interactive)
-  (unless (eolp)
-    (end-of-line))
-  (newline-and-indent))
+;;
+;; C++ specific customizations
+(add-hook 'c-mode-common-hook
+          (lambda()
+            (local-set-key  (kbd "C-c o") 'ff-find-other-file)))
 
-(defun vi-open-line (&optional abovep)
-  "Insert a newline below the current line and put point at beginning.
-With a prefix argument, insert a newline above the current line."
-  (interactive "P")
-  (if abovep
-      (vi-open-line-above)
-    (vi-open-line-below)))
-
+;;
+;; Functions
+;;
 (defun ido-goto-symbol (&optional symbol-list)
   "Refresh imenu and jump to a place in the buffer using Ido."
   (interactive)
@@ -102,3 +87,28 @@ With a prefix argument, insert a newline above the current line."
                     (string= (car imenu--rescan-item) name))
           (add-to-list 'symbol-names name)
           (add-to-list 'name-and-pos (cons name position))))))))
+
+;; Behave like vi's o command
+(defun open-next-line (arg)
+  "Move to the next line and then opens a line.
+    See also `newline-and-indent'."
+  (interactive "p")
+  (end-of-line)
+  (open-line arg)
+  (next-line 1)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+
+;; Behave like vi's O command
+(defun open-previous-line (arg)
+  "Open a new line before the current one.
+     See also `newline-and-indent'."
+  (interactive "p")
+  (beginning-of-line)
+  (open-line arg)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+
+;; Autoindent open-*-lines
+(defvar newline-and-indent t
+  "Modify the behavior of the open-*-line functions to cause them to autoindent.")
