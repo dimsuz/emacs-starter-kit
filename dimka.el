bda758807df332a5e480c8dcabe626f5578c9bfa
Dmitry Suzdalev <dimsuz@gmail.com>
2012-02-06 12:45:53 +0400

    Do not use auto-complete

--- dimka.el
+++ dimka.el
@@ -15,11 +15,6 @@
 (global-set-key (kbd "M-o") 'open-previous-line)
 (global-set-key (kbd "C-o") 'open-next-line)
 
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/auto-complete")
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "/usr/share/emacs/site-lisp/auto-complete/ac-dict")
-(ac-config-default)
-
 ;;
 ;; Generic hook customisations
 ;;
